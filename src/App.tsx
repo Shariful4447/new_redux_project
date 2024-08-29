9d8598de7cd9d78278964facd47772b1a41e3cf5
Shariful4447 <shariful.islam4447@gmail.com>
2024-08-29 21:50:39 +0200

    redux store create started

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 function App() {
   return (
-    <>
-      <div className="text-3xl">
-        <p>React + Redux</p>
+    <div className="h-screen w-full flex mx-auto justify-center items-center">
+      <div className="text-3xl flex border border-purple-300 rounded-md bg-slate-50 p-10">
+        <button className="bg-blue-500 border-2 p-5">Increment</button>
+        <h1 className="p-5">0</h1>
+        <button className="bg-red-500 border-2 p-5">Decrement</button>
       </div>
-    </>
+    </div>
   );
 }
 
